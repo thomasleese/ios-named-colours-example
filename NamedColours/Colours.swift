aad485580fd3f77ad4a9df74d05a17f9601ce558
Thomas Leese <thomas@leese.io>
2018-11-17 14:39:06 +0000

    Add code

---
+++ NamedColours/Colours.swift
@@ -0,0 +1,19 @@
+//
+//  Colours.swift
+//  NamedColours
+//
+//  Created by Thomas Leese on 17/11/2018.
+//  Copyright © 2018 Thomas Leese. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+    static let accent = UIColor(
+        named: "Accent",
+        in: Bundle(for: AppDelegate.self),
+        compatibleWith: nil
+    )
+
+}
